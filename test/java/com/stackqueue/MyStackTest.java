8553be9310bd4dc4718dc6734694432080fe098e
Donally77 <donallypatel0707@gmail.com>
2021-03-12 12:47:31 +0530

    pop stack

--- test/java/com/stackqueue/MyStackTest.java
+++ test/java/com/stackqueue/MyStackTest.java
@@ -20,4 +20,23 @@ public class MyStackTest {
         INode peak = myStack.peak();
         assertEquals(thirdNode, peak);
     }
+
+    @Test
+    public void stackTestForPop() {
+        System.out.println("This test is for popping elements from the Stack.");
+        Node<Integer> firstNode = new Node<>(70);
+        Node<Integer> secondNode = new Node<>(30);
+        Node<Integer> thirdNode = new Node<>(56);
+        MyStack myStack = new MyStack();
+        myStack.push(firstNode);
+        myStack.push(secondNode);
+        myStack.push(thirdNode);
+        myStack.printStack();
+        INode peak = myStack.peak();
+        assertEquals(thirdNode, peak);
+        INode temp = myStack.pop();
+        myStack.printStack();
+        assertEquals(thirdNode, temp);
+    }
+
 }
