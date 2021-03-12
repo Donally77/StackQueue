26db8793a3435b83aac9bd37c67f02d915ac8bdc
Donally77 <donallypatel0707@gmail.com>
2021-03-12 12:30:28 +0530

    welcome message

---
+++ main/java/com/stackqueue/MyStack.java
@@ -0,0 +1,8 @@
+package com.stackqueue;
+
+public class MyStack {
+    public static void main(String[] args){
+        //master welcome message
+        System.out.println("..Welcome to Stack and Queue program..");
+    }
+}
